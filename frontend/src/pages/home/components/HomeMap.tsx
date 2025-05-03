e9c42f2237cbaa29c5b95976f399a7d0cde0b1d1
Gabriel Volpini <gabrielvolpini@192.168.0.12>
2025-05-03 15:17:58 -0300

    Feat: add leafleat

--- frontend/src/pages/home/components/HomeMap.tsx
+++ frontend/src/pages/home/components/HomeMap.tsx
@@ -1,6 +1,14 @@
-export const HomeMap = () => {
-    return(
-        <div>oi</div>
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export function HomeMap() {
+    return (
+        <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.jpg"
+            />
+        </MapContainer>
     )
 }
 
